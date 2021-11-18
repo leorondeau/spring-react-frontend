f47949ef5224e54d50a86b48189c05e751c593c0
Leo Rondeau <christopherleorondeau@gmail.com>
2021-11-18 15:24:11 -0600

    set up application views

---
+++ src/components/React-Spring.js
@@ -0,0 +1,14 @@
+import { Route } from "react-router-dom"
+import React, { useState } from "react"
+
+export const ApplicationViews = (props) => {
+    const [value, setValue] = useState(false)
+
+    return <>
+        <main style={{
+            margin: "5rem 2rem",
+            backgroundColor: "lightgoldenrodyellow"
+        }}>
+        </main>
+    </>
+}
