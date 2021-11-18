16e44cdb9679b90f8ff58068228945929128d2be
Leo Rondeau <christopherleorondeau@gmail.com>
2021-11-18 15:39:00 -0600

    added route to ReactSpring

--- src/components/ReactSpring.js
+++ src/components/ReactSpring.js
@@ -2,4 +2,11 @@ import React from 'react'
 import {Route, Redirect } from "react-router-dom"
 import { ApplicationViews } from './ApplicationViews'
 
-export const ReactSpring
+export const ReactSpring = ()=>{
+    <>
+        <Route render={() => {
+            <Route render={props => <ApplicationViews {...props} />} />
+        }}/>
+    </>
+
+}
