b9840dbaa6f8168a1891d96fbd2cff850e98f04d
Leo Rondeau <christopherleorondeau@gmail.com>
2021-11-18 21:48:04 -0600

    added context provider

---
+++ src/components/employee/EmployeeProvider.js
@@ -0,0 +1,24 @@
+import React, {useState} from 'react'
+
+export const EmployeeContext = React.createContext()
+
+export const EmployeeProvider = (props) => {
+    const[employees, setEmployees]= useState([])
+
+    const getEmployees = () => {
+        return fetch("http://localhost:8080/api/v1/employees",
+        {
+            headers:
+            'Content-type: application/json'}
+    )
+    .then(response => response.json())
+    .then(setEmployees)
+}
+
+return (
+    <EmployeeContext.Provider
+    value={{employees, setEmployees}}>
+        {props.children}
+    </EmployeeContext.Provider>
+)
+}
