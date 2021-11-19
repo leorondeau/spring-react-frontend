9be7f3dccc3c43348c6df85da0b875c492b08d0c
Leo Rondeau <christopherleorondeau@gmail.com>
2021-11-19 10:57:21 -0600

    troubleshooting router config

--- src/components/ReactSpring.js
+++ src/components/ReactSpring.js
@@ -1,14 +1,16 @@
 import React from 'react'
-import {Route, Redirect } from "react-router-dom"
+import { Route, Redirect } from "react-router-dom"
 import { ApplicationViews } from './ApplicationViews'
+import { EmployeeProvider } from './employee/EmployeeProvider'
+import { NavBar } from './nav/NavBar'
 
-export const ReactSpring = ()=>{
-    <>
-        <Route render={() => {
-            return <>
-            <Route render={props => <ApplicationViews {...props} />} />
-            </>
-        }}/>
-    </>
 
+export const ReactSpring = () => {
+    return (
+        <>
+            <NavBar />
+            {/* <h1>Honey Rae's Repair Shop</h1> */}
+            <ApplicationViews />
+        </>
+    )
 }
