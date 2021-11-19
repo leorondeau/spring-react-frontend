56ce6451e0f79d98120c4622b7186f10980a6768
Leo Rondeau <christopherleorondeau@gmail.com>
2021-11-19 16:08:40 -0600

    troubleshooting router config

--- src/components/employee/EmployeeProvider.js
+++ src/components/employee/EmployeeProvider.js
@@ -1,24 +1,21 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
 export const EmployeeContext = React.createContext()
 
 export const EmployeeProvider = (props) => {
-    const[employees, setEmployees]= useState([])
+    const [employees, setEmployees] = useState([])
 
     const getEmployees = () => {
-        return fetch("http://localhost:8080/api/v1/employees",
-        {
-            headers:
-            'Content-type: application/json'}
+        return fetch("http://localhost:8080/api/v1/employees")
+            .then(response => response.json())
+            .then(setEmployees)
+    }
+    
+    console.log("employeesContext", employees)
+    return (
+        <EmployeeContext.Provider
+            value={{ employees, getEmployees }}>
+            {props.children}
+        </EmployeeContext.Provider>
     )
-    .then(response => response.json())
-    .then(setEmployees)
-}
-
-return (
-    <EmployeeContext.Provider
-    value={{employees, setEmployees, getEmployees}}>
-        {props.children}
-    </EmployeeContext.Provider>
-)
 }
