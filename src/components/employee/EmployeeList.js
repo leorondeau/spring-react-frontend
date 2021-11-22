451b2e44a6c2ad4a733d254288a212ed5e95fdbe
Leo Rondeau <christopherleorondeau@gmail.com>
2021-11-22 10:48:14 -0600

    adding bootstrap table

--- src/components/employee/EmployeeList.js
+++ src/components/employee/EmployeeList.js
@@ -9,15 +9,20 @@ export const EmployeeList = (props) => {
         getEmployees()
 
     }, [])
+    
 console.log("employeesList", employees)
     return (
         <>
             <section>
+                <h2 className="text-center">Employees List</h2>
+                <div className = "row">
+                    <table></table>
                 {
                     employees.map(e => (
                         <div key={e.id} value={e.id}>{e.name}</div>
-                    ))
-                }
+                        ))
+                    }
+                    </div>
             </section>
         </>
     )
