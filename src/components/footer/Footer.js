f8f4c4fbd13a141cd5f65446056938152cc39e2e
Leo Rondeau <christopherleorondeau@gmail.com>
2021-11-30 08:44:38 -0600

    added footer component:

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
+import './Footer.css'
 
-export default class Footer extends Component {
-    render() {
+export const Footer = (props)  => {
+  
         return (
             <div>
-                
+                <footer className="footer">
+                    <span className="text-muted">All Rights Reserved 2021 @javaguides</span>
+                </footer>
             </div>
         )
     }
-}
+
+
